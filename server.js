a7b7def97a329d77f9bfbec933659d657f7397e2
zhongjun <63734333@163.com>
2015-10-12 07:59:45 +0800

    Fix issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 var fs = require('fs');
 
-var start = new Date().getMilliseconds() / 1000;
+var start = new Date().getTime() / 1000;
 var sourceArr = [];
 var bakArr = [];
 
@@ -15,13 +15,13 @@ server.on('listening', function () {
 
 server.on('message', function (message, remote) {
   var data = message;
-  var curr = new Date().getMilliseconds() / 1000;
-  if (curr - start > 30) {
+  var curr = new Date().getTime() / 1000;
+  if (curr - start > 3600) {
     start = curr;
     bakArr = sourceArr;
     sourceArr = [];
 
-    fs.writeFile('data.txt', JSON.stringify(bakArr), function (err) {
+    fs.writeFile('htdocs/data.txt', JSON.stringify(bakArr), function (err) {
       if (err) throw err;
       console.log('It\'s saved!');
     });
@@ -32,7 +32,7 @@ server.on('message', function (message, remote) {
       y: data.readFloatBE(34)
     });
   }
-  console.log(sourceArr.length);
+  //console.log(sourceArr.length);
 });
 
 server.bind(PORT);
