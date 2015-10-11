7bc92a26b9a85e0490c66e7a36e61fdd8e422a9d
zhongjun <63734333@163.com>
2015-10-12 07:39:47 +0800

    Fix issue

--- server.js
+++ server.js
@@ -2,15 +2,39 @@ var PORT = 33333;
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
+var fs = require('fs');
+
+var start = new Date().getSeconds();
+var sourceArr = [];
+var bakArr = [];
 
 server.on('listening', function () {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+  var address = server.address();
+  console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 
 server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
+  console.log(remote.address + ':' + remote.port + ' - ' + message);
 
+  var data = message;
+  var curr = new Date().getSeconds();
+  if (curr - start > 30) {
+    start = curr;
+    bakArr = sourceArr;
+    sourceArr = [];
+
+    fs.writeFile('data.txt', bakArr, function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
+  } else {
+    sourceArr.push({
+      mapId: data.readIntBE(4, 4),
+      x: data.readFloatBE(30),
+      y: data.readFloatBE(34)
+    });
+  }
+  console.log(sourceArr.length);
 });
 
 server.bind(PORT);
